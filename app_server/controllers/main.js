b5d49f79da035a535059ce97f49e61b0caa90b43
Josh Seipel <joshseipel@gmail.com>
2017-05-28 16:29:44 -0400

    Separated routes and controllers

---
+++ app_server/controllers/main.js
@@ -0,0 +1,6 @@
+// Controller
+
+// GET home page
+module.exports.index = function(req, res){
+	res.render('index', { title: 'Express'});
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,15 +1,20 @@
+// Router
+
 var express = require('express');
 var router = express.Router();
+var ctrlMain = require('../controllers/main');
 
 // Created a named function, and then passed it
 // through as a callback in the route definition.
+// The controller should handle the app logic while
+// the router maps Url requests to controllers.
 
-var homePageController = function(req, res) {
-	res.render('index', { title: 'Express' });
-}
+// var homePageController = function(req, res) {
+// 	res.render('index', { title: 'Express' });
+// }
 
 /* GET home page. */
-router.get('/', homePageController);
+router.get('/', ctrlMain.index);
 
 
 

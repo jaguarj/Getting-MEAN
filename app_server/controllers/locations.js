8aac80fa075bf464f672cb6dda30268623606375
Josh Seipel <joshseipel@gmail.com>
2017-05-28 21:29:22 -0400

    Finished styling locations-info page.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,7 +5,7 @@ module.exports.homelist = function(res, res) {
 
 // GET 'locations' page
 module.exports.locationInfo = function(req, res) {
-	res.render('index', { title: 'Location Info' });
+	res.render('location-info', { title: 'Location Info' });
 };
 
 // GET 'Add review' page

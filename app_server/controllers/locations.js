576ac8966ae7fbee2f966c6c4f9f5dcffc1a9a92
Josh Seipel <joshseipel@gmail.com>
2017-05-30 16:22:28 -0400

    Added locations object to the controller, and changed the view to rep this data.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,7 +5,26 @@ module.exports.homelist = function(req, res) {
 		pageHeader: {
 			title: 'Loca8r',
 			strapline: 'Find places to work with wifi near you!'
-		}
+		},
+		locations: [{
+			name: 'Starcups',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 3,
+			facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+			distance: '100m'
+		}, {
+			name: 'Cafe Hero',
+			address: '325 High Street, Reading, RG6 1PS',
+			rating: 4,
+			facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+			distance: '200m'
+		}, {
+			name: 'Burger Queen',
+			address: '234 Read Street, Sun Valley, RG6 2PS',
+			rating: 5,
+			facilities: ['Food', 'Premium Wifi'],
+			distance: '500m'
+		}]
 	});
 };
 

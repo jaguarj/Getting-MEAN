32c1749d19a41c29021f4519ef1e61b90b1a3689
Josh Seipel <joshseipel@gmail.com>
2017-05-30 20:39:35 -0400

    Added notifications with gracefulShutdown func when app disconnects from mongoose.

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var gracefulShutdown;
 var dbURI = 'mongodb://localhost/Loc8r';
 mongoose.connect(dbURI);
 
@@ -10,4 +11,31 @@ mongoose.connection.on('error', function (err) {
 });
 mongoose.connection.on('disconnected', function () {
 	console.log('Mongoose disconnected!');
+});
+
+
+gracefulShutdown = function (msg, callback) {
+	mongoose.connection.close(function () {
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+	});
+};
+
+// For nodemon restarts
+process.once('SIGUSR2', function () {
+	gracefulShutdown('nodemon restart', function () {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+//  For app termination
+process.on('SIGINT', function () {
+	gracefulShutdown('app termination', function () {
+		process.exit(0);
+	});
+});
+// For Heroku app termination
+process.on('SIGTERM', function () {
+	gracefulShutdown('Heroku app shutdown', function () {
+		process.exit(0);
+	});
 });

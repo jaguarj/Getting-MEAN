2a95ace418b3d7444fbd33ca6dcf1277b0240648
Josh Seipel <joshseipel@gmail.com>
2017-06-12 15:50:37 -0400

    Making locations and reviews api placeholder routes.

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,35 @@
+var sendJsonResponse = function(res, status, content) {
+	res.status(status);
+	res.json(content);
+};
+
+module.exports.locationsListByDistance = function(req, res) {
+	sendJsonResponse(res, 200, {'status' : 'success'});
+};
+
+module.exports.locationsCreate = function(req, res) {
+	sendJsonResponse(res, 200, {'status' : 'success'});
+};
+
+module.exports.locationsReadOne = function(req, res) {
+	sendJsonResponse(res, 200, {'status' : 'success'});
+};
+
+module.exports.locationsUpdateOne = function(req, res) {
+	sendJsonResponse(res, 200, {'status' : 'success'});
+};
+
+module.exports.locationsDeleteOne = function(req, res) {
+	sendJsonResponse(res, 200, {'status' : 'success'});
+};
+
+
+
+
+
+
+// router.get('/locations', ctrlLocations.locationsListByDistance);
+// router.post('/locations', ctrlLocations.locationsCreate);
+// router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
+// router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
+router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);

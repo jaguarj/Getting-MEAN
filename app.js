6392d4bd7c77a43b49d53afc6fa3775f6e5c7919
Josh Seipel <joshseipel@gmail.com>
2017-06-12 16:40:08 -0400

    Chagned require in app.js to look for models in app_api.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 // var index = require('./app_server/routes/index');
 var routes = require('./app_server/routes/index');
@@ -28,6 +28,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// Api routes
+app.use('/api', routesApi);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

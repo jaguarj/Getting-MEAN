5333d9fcfef5d6aa07a0139b0f3a15ec4d7a4c8c
Josh Seipel <joshseipel@gmail.com>
2017-06-12 15:09:04 -0400

    Making index.js for api routes.

---
+++ app_api/routes/index.js
@@ -0,0 +1,6 @@
+// Api index of routes
+var express = require('express');
+var router = express.Router();
+var ctrlLocations = require('../controllers/locations');
+var ctrlReviews = require('../controlers/reviews');
+

d7730202bd68e8d1cf205272e0db57bd37d4eaab
Josh Seipel <joshseipel@gmail.com>
2017-06-12 14:52:16 -0400

    Updated app.js from var index to var routes.

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 require('./app_server/models/db');
 
-var index = require('./app_server/routes/index');
+// var index = require('./app_server/routes/index');
+var routes = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
+
+var routesApi = require('./app_api/routes/index');
 
 var app = express();
 
@@ -23,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler

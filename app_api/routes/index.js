f5b09091314fc404c1b31110642d23dd0ff10b0b
Josh Seipel <joshseipel@gmail.com>
2017-06-12 15:27:51 -0400

    Done making api routes.

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,3 +4,19 @@ var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
 var ctrlReviews = require('../controlers/reviews');
 
+// Locations
+router.get('/locations', ctrlLocations.locationsListByDistance);
+router.post('/locations', ctrlLocations.locationsCreate);
+router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
+router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
+router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
+
+// Reviews
+router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
+router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
+router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
+router.delete('/locations/:locationid/reviews/:reviewsid', ctrlReviews.reviewsDeleteOne);
+
+module.exports = router;
+
+
